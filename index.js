84a16901b96e8e93811a90e244b8286bb4b423ed
Brigid Wanza Wambua <wanzanashami@gmail.com>
2024-03-31 14:44:44 +0300

    feat: ...

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, let's ${activity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun('learn code'));
+
+
+
+
+
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, let's go to the office!`;
+};
+
+// Define wrapAdjective function
+function wrapAdjective(flair = '*') {
+    return function (adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+
